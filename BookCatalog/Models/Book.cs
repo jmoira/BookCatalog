20d7ef4f6a43116e7fbcac506d2a57fad3ac8420
jmoira <jen.mcquerry98@gmail.com>
2022-09-26 17:21:35 -0400

    Add project files.

---
+++ BookCatalog/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace BookCatalog.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        [Range(1,10, ErrorMessage= "Rating must be between 1 and 10")]
+        public int Rating { get; set; }
+    }
+}
